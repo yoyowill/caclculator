9a4573826f7177f203438acd0737383542265497
Fanny <zhaoshuyan618.163.com>
2016-05-24 13:37:53 +0800

    version 0.3

--- ssss/ssss/ViewController.swift
+++ ssss/ssss/ViewController.swift
@@ -29,11 +29,6 @@ class ViewController: UIViewController {
     @IBAction func enter() {
     }
 
-    @IBAction func enter() {
-    }
-    @IBAction func enter() {
-    }
-    @IBAction func enter() {
-    }
+
 }
 
